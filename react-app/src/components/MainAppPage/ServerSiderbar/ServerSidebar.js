2d4c8c56deac95c26ae579e3024b0902d5fd51f5
dsomorov <dsomorov@gmail.com>
2023-07-01 13:01:57 -0500

    style server sidebar

--- react-app/src/components/MainAppPage/ServerSiderbar/ServerSidebar.js
+++ react-app/src/components/MainAppPage/ServerSiderbar/ServerSidebar.js
@@ -3,7 +3,38 @@ import "./ServerSidebar.css"
 export default function ServerSidebar() {
   return (
     <div className="server-sidebar">
-      Hello server
+      <ul className="server-list">
+        <li className="server-icon">
+          <img
+            className="server-icon__image"
+            src="image.png"
+            alt=""
+          />
+          <span className="server-icon__tooltip">
+            Server 1 Name
+          </span>
+        </li>
+        <li className="server-icon">
+          <img
+            className="server-icon__image"
+            src="image.png"
+            alt=""
+          />
+          <span className="server-icon__tooltip">
+            Server 1 Name
+          </span>
+        </li>
+        <li className="server-icon">
+          <img
+            className="server-icon__image"
+            src="image.png"
+            alt=""
+          />
+          <span className="server-icon__tooltip">
+            Server 1 Name
+          </span>
+        </li>
+      </ul>
     </div>
   )
 }
