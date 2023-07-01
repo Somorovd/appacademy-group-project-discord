4710475213d6c6a74e98629a359acc603793b60d
dsomorov <dsomorov@gmail.com>
2023-06-30 19:23:26 -0500

    updated signup form fields

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -4,11 +4,20 @@ import { Redirect } from "react-router-dom";
 import { signUp } from "../../store/session";
 import './SignupForm.css';
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const startYear = 1950
+const currentYear = new Date().getFullYear()
+const numYears = currentYear - startYear;
+
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -18,12 +27,23 @@ function SignupFormPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
-        const data = await dispatch(signUp(username, email, password));
-        if (data) {
-          setErrors(data)
-        }
+
+      const birthday = new Date(`${month} ${day}, ${year}`)
+
+      const user_obj = {
+        username,
+        email,
+        phoneNumber,
+        birthday,
+        password
+      }
+
+      const data = await dispatch(signUp(user_obj));
+      if (data) {
+        setErrors(data)
+      }
     } else {
-        setErrors(['Confirm Password field must be the same as the Password field']);
+      setErrors(['Confirm Password field must be the same as the Password field']);
     }
   };
 
@@ -53,6 +73,65 @@ function SignupFormPage() {
           />
         </label>
         <label>
+          Phone Number
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Date of Birth
+          <div>
+            <select
+              value={month}
+              onChange={(e) => setMonth(e.target.value)}
+            >
+              <option value={""} hidden disabled>Month</option>
+              {
+                months.map((month, i) => {
+                  return (
+                    <option value={i + 1}>
+                      {month}
+                    </option>
+                  )
+                })
+              }
+            </select>
+            <select
+              value={day}
+              onChange={(e) => setDay(e.target.value)}
+            >
+              <option value={""} hidden disabled>Day</option>
+              {
+                Array.from({ length: 31 }, (_, i) => {
+                  return (
+                    <option value={i + 1}>
+                      {i + 1}
+                    </option>
+                  )
+                })
+              }
+            </select>
+            <select
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            >
+              <option value={""} hidden disabled>Year</option>
+              {
+                Array.from({ length: numYears }, (_, i) => {
+                  return (
+                    <option value={currentYear - i}>
+                      {currentYear - i}
+                    </option>
+                  )
+                })
+              }
+            </select>
+          </div>
+        </label>
+        <label>
           Password
           <input
             type="password"
@@ -71,7 +150,7 @@ function SignupFormPage() {
           />
         </label>
         <button type="submit">Sign Up</button>
-      </form>
+      </form >
     </>
   );
 }
