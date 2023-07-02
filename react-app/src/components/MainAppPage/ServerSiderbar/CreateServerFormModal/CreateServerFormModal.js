e8874b8d95759530918c11c51a4675eb4a22c7cb
dsomorov <dsomorov@gmail.com>
2023-07-02 13:11:08 -0500

    create new server thunk

--- react-app/src/components/MainAppPage/ServerSiderbar/CreateServerFormModal/CreateServerFormModal.js
+++ react-app/src/components/MainAppPage/ServerSiderbar/CreateServerFormModal/CreateServerFormModal.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../../../context/Modal";
+
+import * as serverActions from "../../../../store/servers";
 import "./CreateServerFormModal.css"
 
 export default function CreateServerFormModal() {
@@ -8,11 +10,23 @@ export default function CreateServerFormModal() {
   const user = useSelector((state) => state.session.user)
   const [name, setName] = useState(`${user.username}'s server`);
   const [image, setImage] = useState("");
+  const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const serverObject = {
+      name,
+      image,
+      private: true,
+      about: ""
+    }
+
+    const data = await dispatch(serverActions.actionCreateServer(serverObject));
+    if (data) setErrors(data);
+
     closeModal()
   }
 
--- react-app/src/store/servers.js
+++ react-app/src/store/servers.js
@@ -1,6 +1,8 @@
 
 const GET_USER_SERVERS = "servers/GET_USER_SERVERS";
-const GET_SINGLE_SERVER = "servers/GET_SINGLE_SERVER"
+const GET_SINGLE_SERVER = "servers/GET_SINGLE_SERVER";
+const CREATE_SERVER = "servers/CREATE_SERVER";
+
 
 const actionGetAllUserServers = (servers) => ({
   type: GET_USER_SERVERS,
@@ -9,6 +11,11 @@ const actionGetAllUserServers = (servers) => ({
 
 const actionGetSingleServer = (server) => ({
   type: GET_SINGLE_SERVER,
+  payload: server
+})
+
+const actionCreateServer = (server) => ({
+  type: CREATE_SERVER,
   payload: server
 })
 
@@ -35,6 +42,28 @@ export const thunkGetSingleServer = (serverId) => async (dispatch) => {
   }
 }
 
+export const thunkCreateServer = (server) => async (dispatch) => {
+  const res = await fetch(`/api/servers/new`, {
+    method: "post",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(server)
+  });
+  const resBody = await res.json();
+
+  if (res.ok) {
+    const server = resBody;
+    dispatch(actionCreateServer(server));
+  } else if (res.status < 500) {
+    if (resBody.errors) {
+      return resBody.errors;
+    }
+  } else {
+    return ["An error occurred. Please try again."];
+  }
+}
+
 const initialState = {
   allServers: {},
   allUserServers: {},
@@ -44,9 +73,13 @@ const initialState = {
 export default function serversReducer(state = initialState, action) {
   switch (action.type) {
     case GET_USER_SERVERS:
-      return { ...state, allUserServers: action.payload }
+      return { ...state, allUserServers: action.payload };
     case GET_SINGLE_SERVER:
-      return { ...state, singleUserServer: action.payload }
+      return { ...state, singleUserServer: action.payload };
+    case CREATE_SERVER:
+      const newState = { ...state, singleUserServer: action.payload };
+      newState.allUserServers[action.payload.id] = action.payload;
+      return newState;
     default:
       return state;
   }
