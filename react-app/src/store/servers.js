90daaa6ce722df190373ca2b18b2dcb382498dc0
Christian Spada <spada17@gmail.com>
2023-07-03 17:06:37 -0400

    update store state for create channel

--- react-app/src/store/servers.js
+++ react-app/src/store/servers.js
@@ -1,38 +1,42 @@
+const GET_USER_SERVERS = 'servers/GET_USER_SERVERS';
+const GET_SINGLE_SERVER = 'servers/GET_SINGLE_SERVER';
+const CREATE_SERVER = 'servers/CREATE_SERVER';
+const CREATE_CHANNEL = 'servers/CREATE_CHANNEL';
 
-const GET_USER_SERVERS = "servers/GET_USER_SERVERS";
-const GET_SINGLE_SERVER = "servers/GET_SINGLE_SERVER";
-const CREATE_SERVER = "servers/CREATE_SERVER";
-
-
-const actionGetAllUserServers = (servers) => ({
+const actionGetAllUserServers = servers => ({
   type: GET_USER_SERVERS,
-  payload: servers
-})
+  payload: servers,
+});
 
-const actionGetSingleServer = (server) => ({
+const actionGetSingleServer = server => ({
   type: GET_SINGLE_SERVER,
-  payload: server
-})
+  payload: server,
+});
 
-const actionCreateServer = (server) => ({
+const actionCreateServer = server => ({
   type: CREATE_SERVER,
-  payload: server
-})
+  payload: server,
+});
 
-export const thunkGetAllUserServers = () => async (dispatch) => {
-  const res = await fetch("/api/servers/current");
+const actionCreateChannel = channel => ({
+  type: CREATE_CHANNEL,
+  payload: channel,
+});
+
+export const thunkGetAllUserServers = () => async dispatch => {
+  const res = await fetch('/api/servers/current');
   const resBody = await res.json();
 
   if (res.ok) {
-    const servers = {}
+    const servers = {};
     for (let server of resBody.servers) {
       servers[server.id] = server;
     }
     dispatch(actionGetAllUserServers(servers));
   }
-}
+};
 
-export const thunkGetSingleServer = (serverId) => async (dispatch) => {
+export const thunkGetSingleServer = serverId => async dispatch => {
   const res = await fetch(`/api/servers/${serverId}`);
   const resBody = await res.json();
 
@@ -40,15 +44,15 @@ export const thunkGetSingleServer = (serverId) => async (dispatch) => {
     const server = resBody;
     dispatch(actionGetSingleServer(server));
   }
-}
+};
 
-export const thunkCreateServer = (server) => async (dispatch) => {
+export const thunkCreateServer = server => async dispatch => {
   const res = await fetch(`/api/servers/new`, {
-    method: "post",
+    method: 'post',
     headers: {
-      "content-type": "application/json"
+      'content-type': 'application/json',
     },
-    body: JSON.stringify(server)
+    body: JSON.stringify(server),
   });
   const resBody = await res.json();
 
@@ -60,15 +64,38 @@ export const thunkCreateServer = (server) => async (dispatch) => {
       return resBody.errors;
     }
   } else {
-    return ["An error occurred. Please try again."];
+    return ['An error occurred. Please try again.'];
   }
-}
+};
+
+export const thunkCreateChannel = (channel, serverId) => async dispatch => {
+  const res = await fetch(`/api/servers/${serverId}/channels`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(channel),
+  });
+
+  const resBody = await res.json();
+
+  if (res.ok) {
+    const channel = resBody;
+    dispatch(actionCreateChannel(channel));
+  } else if (res.status < 500) {
+    if (resBody.errors) {
+      return resBody.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.'];
+  }
+};
 
 const initialState = {
   allServers: {},
   allUserServers: {},
-  singleUserServer: {}
-}
+  singleUserServer: {},
+};
 
 export default function serversReducer(state = initialState, action) {
   switch (action.type) {
@@ -79,9 +106,20 @@ export default function serversReducer(state = initialState, action) {
     case CREATE_SERVER:
       const allUserServers = {
         ...state.allUserServers,
-        [action.payload.id]: action.payload
-      }
+        [action.payload.id]: action.payload,
+      };
       return { ...state, allUserServers, singleUserServer: action.payload };
+    case CREATE_CHANNEL:
+      return {
+        ...state,
+        singleUserServer: {
+          ...state.singleUserServer,
+          channels: {
+            ...state.singleUserServer.channels,
+            [action.payload.id]: action.payload,
+          },
+        },
+      };
     default:
       return state;
   }
