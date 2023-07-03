22c612d1b10bfd016fbfe83ca80f6ccd8353b23f
Christian Spada <spada17@gmail.com>
2023-07-03 13:00:38 -0400

    changed dm and explore icon components into divs

--- react-app/src/components/MainAppPage/ServerSiderbar/ServerSidebar.js
+++ react-app/src/components/MainAppPage/ServerSiderbar/ServerSidebar.js
@@ -1,66 +1,52 @@
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-import * as serverActions from "../../../store/servers"
-import ServerSidebarIcon from "./ServerSidebarIcon";
-import OpenModalButton from "../../OpenModalButton";
-import "./ServerSidebar.css"
-import CreateServerFormModal from "./CreateServerFormModal";
-import LoginFormModal from "../../LoginFormModal";
-
-const directMessageLink = {
-  name: "Direct Messages",
-  image: "",
-}
-
-const addServerLink = {
-  name: "Add a Server",
-  image: ""
-}
-
-const browseServersLink = {
-  name: "Explore Discoverable Servers",
-  image: ""
-}
+import * as serverActions from '../../../store/servers';
+import ServerSidebarIcon from './ServerSidebarIcon';
+import OpenModalButton from '../../OpenModalButton';
+import './ServerSidebar.css';
+import CreateServerFormModal from './CreateServerFormModal';
 
 export default function ServerSidebar() {
   const dispatch = useDispatch();
-  const allUserServers = useSelector((state) => state.servers.allUserServers);
+  const history = useHistory();
+  const allUserServers = useSelector(state => state.servers.allUserServers);
   const userServers = Object.values(allUserServers);
 
   useEffect(() => {
-    dispatch(serverActions.thunkGetAllUserServers())
-  }, [dispatch])
-
+    dispatch(serverActions.thunkGetAllUserServers());
+  }, [dispatch]);
 
   return (
     <div className="server-sidebar">
       <ul className="server-list">
-        <ServerSidebarIcon
-          server={directMessageLink}
-          nav="/main/conversations"
-          className="server-icon--messages"
-        />
-        {
-          userServers.map((server, i) => (
-            <ServerSidebarIcon
-              server={server}
-              nav={`/main/channels/${server.id}`}
-              key={i} />
-          ))
-        }
-        <div className="server-icon server-icon--add-server">
-          <OpenModalButton
-            modalComponent={<CreateServerFormModal />}
+        <div
+          className="server-icon server-icon--messages"
+          onClick={() => history.push('/main/conversations')}
+        >
+          <span className="server-icon__tooltip">Direct Messages</span>
+        </div>
+        {userServers.map((server, i) => (
+          <ServerSidebarIcon
+            server={server}
+            nav={`/main/channels/${server.id}`}
+            key={i}
           />
+        ))}
+        <div className="server-icon server-icon--add-server">
+          <OpenModalButton modalComponent={<CreateServerFormModal />} />
           <span className="server-icon__tooltip">Add Server</span>
         </div>
-        <ServerSidebarIcon
-          server={browseServersLink}
-          nav="/main/channels"
-          className="server-icon--browse-servers"
-        />
+        <div
+          className="server-icon server-icon--browse-servers"
+          onClick={() => history.push('/main/conversations')}
+        >
+          <span className="server-icon__tooltip">
+            Explore Discoverable Servers
+          </span>
+        </div>
       </ul>
     </div>
-  )
+  );
 }
