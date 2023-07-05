c8af00204c70bf180cdbcfc03e697e6d672ee030
dsomorov <dsomorov@gmail.com>
2023-07-05 12:35:39 -0500

    add backend get channel details endpoint

---
+++ app/api/channel_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.models import db, Channel, Message
+
+
+channel_routes = Blueprint("channels", __name__)
+
+
+@channel_routes.route("/<int:channel_id>")
+@login_required
+def single_channel(channel_id):
+    """
+    Query for a single channel details by id
+    """
+
+    channel = Channel.query.get(channel_id)
+
+    if channel == None:
+        return
+
+    messages = Message.query.filter(Message.channel_id == channel_id).order_by(
+        Message.created_at
+    )
+
+    channel.messages = messages
+    return channel.to_dict_single()
