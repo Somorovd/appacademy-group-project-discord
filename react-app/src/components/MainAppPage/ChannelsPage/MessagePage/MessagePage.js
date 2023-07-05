29c8917098c14d7ebc7e4ecbf40e66062c6a77c9
dsomorov <dsomorov@gmail.com>
2023-07-05 13:21:46 -0500

    add message input form

--- react-app/src/components/MainAppPage/ChannelsPage/MessagePage/MessagePage.js
+++ react-app/src/components/MainAppPage/ChannelsPage/MessagePage/MessagePage.js
@@ -1,20 +1,65 @@
-import { useSelector } from 'react-redux';
+import { useState, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+
+import * as channelActions from "../../../../store/channels";
 
 import './MessagePage.css'
 
 export default function MessagePage() {
   const { channelId } = useParams();
+  const dispatch = useDispatch();
+  const [content, setContent] = useState("");
 
   const singleChannel = useSelector(
-    state => state.servers.singleUserServer.channels[channelId]
+    state => state.channels.singleChannel
   );
+
+  // const messageInput = useRef();
+
+  // const submitMessage = (e) => {
+  //   if (e.key !== "Enter") return;
+  //   console.log("Validating ", content);
+  //   if (!content.trim()) return;
+  //   console.log("Sending ", content);
+  //   setContent("");
+  // }
+
+  useEffect(() => {
+    dispatch(channelActions.thunkGetChannel(channelId));
+  }, [dispatch, channelId]);
+
+  // useEffect(() => {
+  //   messageInput.current.addEventListener("keydown", submitMessage);
+  //   return () => {
+  //     messageInput.current.removeEventListener("keydown", submitMessage);
+  //     setContent("");
+  //   }
+  // }, [singleChannel, messageInput.current]);
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <div className='channels-messages'>
-      {singleChannel
-        ? `Displaying messages for ${singleChannel.name}`
-        : ''}
+      <div className='message-container'>
+        {singleChannel
+          ? `Displaying messages for ${singleChannel.name}`
+          : ''}
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="message-input"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder={`Message ${singleChannel.name}`}
+          // ref={messageInput}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
