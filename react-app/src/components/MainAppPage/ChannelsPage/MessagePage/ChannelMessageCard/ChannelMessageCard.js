42944c14fb3acdebf0857d09bd28aac8fe603155
Christian Spada <spada17@gmail.com>
2023-07-05 17:16:45 -0400

    add channel message component

---
+++ react-app/src/components/MainAppPage/ChannelsPage/MessagePage/ChannelMessageCard/ChannelMessageCard.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import './ChannelMessageCard.css';
+import { useParams } from 'react-router-dom';
+
+export default function ChannelMessageCard({ message, user, socket }) {
+  const { channelId } = useParams();
+  const [editing, setEditing] = useState(false);
+  const [editMessage, setEditMessage] = useState(message.content);
+
+  const handleDelete = messageId => {
+    console.log('DELETING MESSAGE');
+    socket.emit('messages', {
+      user,
+      content: '',
+      room: `Channel-${channelId}`,
+      edited: false,
+      deleted: messageId,
+    });
+  };
+
+  const handleEdit = (messageId, newContent) => {
+    console.log('EDITING MESSAGE');
+    socket.emit('messages', {
+      user,
+      content: newContent,
+      room: `Channel-${channelId}`,
+      edited: messageId,
+      deleted: false,
+    });
+
+    setEditing(false);
+  };
+
+  const formattedDate = new Date(message.updatedAt).toLocaleDateString(
+    'en-US',
+    {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    }
+  );
+
+  return (
+    <div className="channel-message-card">
+      <div className="DM-page__list-message-user">
+        <img
+          src={message.user.profilePic}
+          alt=""
+        />
+        {message.user.username}{' '}
+        <span className="DM-page__list-message-date">{formattedDate} </span>
+        {message.wasEdited && (
+          <span className="DM-page__list-message-date">- edited</span>
+        )}
+        {user.id === message.user.id && (
+          <>
+            <button onClick={() => setEditing(!editing)}>Edit</button>
+            <button onClick={() => handleDelete(message.id)}>Delete</button>
+          </>
+        )}
+      </div>
+      <div className={`DM-page__list-message-content`}>{message.content}</div>
+      <div className={editing ? '' : 'hidden'}>
+        <input
+          value={editMessage}
+          onChange={e => setEditMessage(e.target.value)}
+        />
+        <button onClick={() => handleEdit(message.id, editMessage)}>
+          Save
+        </button>
+        <button
+          onClick={() => {
+            setEditMessage(message.content);
+            setEditing(false);
+          }}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+}
