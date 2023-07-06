8c9d4c1c17a1a5e450b6345794c146cd642b703d
Christian Spada <spada17@gmail.com>
2023-07-06 10:00:03 -0400

    add leave server modal + style it

--- react-app/src/components/MainAppPage/ChannelsPage/ServerMenu/LeaveServerModal/LeaveServerModal.js
+++ react-app/src/components/MainAppPage/ChannelsPage/ServerMenu/LeaveServerModal/LeaveServerModal.js
@@ -1,7 +1,23 @@
-import './LeaveServerModal.css'
+import './LeaveServerModal.css';
 
-export default function LeaveServerModal() {
+export default function LeaveServerModal({ serverToLeave }) {
   return (
-    <h1> LeaveServerModal </h1>
-  )
+    <div className="leave-server-modal">
+      <header className="leave-server-modal__header">
+        <h2>Leave '{serverToLeave.name}'</h2>
+      </header>
+      <p className="leave-server-modal__warning">
+        Are you sure you want to leave {serverToLeave.name}? You won't be able
+        to rejoin this server unless you are re-invited?
+      </p>
+      <footer className="leave-server-modal__footer">
+        <div className="leave-server-modal__btn-container">
+          <button className="leave-server-modal__cancel-btn">Cancel</button>
+          <button className="leave-server-modal__leave-btn">
+            Leave Server
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 }
